68edcd2dda738053bb8c8dbfa98808c02b3bc795
JYOTI <92581031+yufei20@users.noreply.github.com>
2026-03-16 10:33:08 +0530

    Time: 229 ms (82.41%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,8 +1,7 @@
 SELECT 
-    machine_id,
+    machine_id, 
     ROUND(
-        SUM(CASE WHEN activity_type = 'end' THEN timestamp ELSE -timestamp END) 
-        / COUNT(DISTINCT process_id), 
-    3) AS processing_time
+        SUM(CASE WHEN activity_type = 'end' THEN timestamp ELSE -timestamp END) / COUNT(DISTINCT process_id), 3
+    ) AS processing_time
 FROM Activity
 GROUP BY machine_id;
